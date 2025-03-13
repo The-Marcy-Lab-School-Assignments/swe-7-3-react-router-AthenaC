f15d4f9264ac5e8f4138295bf1d567dee45afdec
Athena Chang <athenachang17@gmail.com>
2025-03-13 18:00:40 -0400

    fixed GoHomeButton

--- app/src/components/GoHomeButton.jsx
+++ app/src/components/GoHomeButton.jsx
@@ -1,13 +1,12 @@
 // TODO:
 // - Import the Link component from React Router
 // - Make the button navigate the user back to the home page
-import { useNavigate, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const GoHomeButton = () => {
-  const navigate = useNavigate();
   return (
     <button className="ui button fluid">
-      <Link to={navigate("/")}>Go Home</Link>
+      <Link to="/">Go Home</Link>
     </button>
   );
 };
